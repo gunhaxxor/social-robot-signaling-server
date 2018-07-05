86f142adf52bb8d286d8001ac8e3ba9164c2e14b
JonasTheCuriousDev <jonjage@gmail.com>
2018-07-05 12:49:49 +0200

    Ändrade i Server.js.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var _ = require('lodash');
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var _ = require("lodash");
 
 let PORT = 3000;
 if (process.env.PORT) {
@@ -10,14 +10,13 @@ if (process.env.PORT) {
 
 var users = [];
 
-console.log('Startar server.js');
-
-app.get('/', function(req, res) {
-  res.sendfile('index.html');
+console.log("Startar server.js");
+app.get("/", function(req, res) {
+  res.sendfile("index.html");
 });
 
-io.on('connection', function(socket) {
-  socket.on('login', function(data) {
+io.on("connection", function(socket) {
+  socket.on("login", function(data) {
     // if this socket is already connected,
     // send a failed login message
     if (
@@ -25,21 +24,28 @@ io.on('connection', function(socket) {
         socket: socket.id
       }) !== -1
     ) {
-      socket.emit('login_error', 'You are already connected.');
+      socket.emit("login_error", "You are already connected.");
     }
 
     users.push({ id: data.id, socket: socket.id });
-    console.log('socket with id ' + socket.id + ' logged in');
+    console.log("socket with id " + socket.id + " logged in");
   });
 
-  socket.on('sendMessage', function(message) {
+  socket.on("sendMessage", function(message) {
+    if (!message.peer_id) {
+      console.log("no peer_id provided!!! Saay whaaaaaaa?!");
+      return;
+    }
     var peer_id = Number(message.peer_id);
     var contact = _.find(users, { id: peer_id });
-
-    io.to(contact.socket).emit('messageReceived', message);
+    if (!contact) {
+      console.log("no such peer found in the user list!");
+      return;
+    }
+    io.to(contact.socket).emit("messageReceived", message);
   });
 
-  socket.on('disconnect', function() {
+  socket.on("disconnect", function() {
     _.remove(users, function(user) {
       return user.socket == socket.id;
     });
@@ -50,6 +56,6 @@ http.listen(PORT, function() {
   var host = http.address().address;
   var port = http.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
   // console.log('listening on *:' + PORT);
 });
