b85aeed69c69456d52b43442a593253ac94a569e
HACKING-STATION\gunhaxxor <gunnar.oledal@gmail.com>
2018-10-18 12:50:53 +0200

    Removing old shit. Cleeeaning!

--- server.js
+++ server.js
@@ -10,27 +10,27 @@ if (process.env.PORT) {
 
 var users = [];
 
-console.log("Startar server.js");
+console.log("Startar social robot signaling server!");
 app.get("/", function(req, res) {
   res.sendfile("index.html");
 });
 
 io.on("connection", function(socket) {
   console.log("socket connection established. id: " + socket.id);
-  socket.on("login", function(data) {
-    // if this socket is already logged in,
-    // send a failed login message
-    if (
-      _.findIndex(users, {
-        socket: socket.id
-      }) !== -1
-    ) {
-      socket.emit("login_error", "You are already connected.");
-    }
+  // socket.on("login", function(data) {
+  //   // if this socket is already logged in,
+  //   // send a failed login message
+  //   if (
+  //     _.findIndex(users, {
+  //       socket: socket.id
+  //     }) !== -1
+  //   ) {
+  //     socket.emit("login_error", "You are already connected.");
+  //   }
 
-    users.push({ id: data.id, socket: socket.id });
-    console.log("socket with id " + socket.id + " logged in");
-  });
+  //   users.push({ id: data.id, socket: socket.id });
+  //   console.log("socket with id " + socket.id + " logged in");
+  // });
 
   // Handle RTC signaling transparently. Just pass on the messageto the other clients
   socket.on("signal", data => {
@@ -39,35 +39,35 @@ io.on("connection", function(socket) {
     socket.broadcast.emit("signal", data);
   });
 
-  socket.on("sendMessage", function(message) {
-    if (!message.peer_id) {
-      console.log("no peer_id provided!!! Saay whaaaaaaa?!");
-      return;
-    }
-    var peer_id = Number(message.peer_id);
-    var contact = _.find(users, { id: peer_id });
-    if (!contact) {
-      console.log("no such peer found in the user list!");
-      return;
-    }
-    console.log(
-      "sending message of type " +
-        message.type +
-        " from " +
-        message.id +
-        " to " +
-        message.peer_id
-    );
-    if (message.data) {
-      console.log("data:" + JSON.stringify(message.data));
-    }
-    console.log("with socketId's: " + socket.id + ", " + contact.socket);
-    io.to(contact.socket).emit("messageReceived", message);
-  });
+  // socket.on("sendMessage", function(message) {
+  //   if (!message.peer_id) {
+  //     console.log("no peer_id provided!!! Saay whaaaaaaa?!");
+  //     return;
+  //   }
+  //   var peer_id = Number(message.peer_id);
+  //   var contact = _.find(users, { id: peer_id });
+  //   if (!contact) {
+  //     console.log("no such peer found in the user list!");
+  //     return;
+  //   }
+  //   console.log(
+  //     "sending message of type " +
+  //       message.type +
+  //       " from " +
+  //       message.id +
+  //       " to " +
+  //       message.peer_id
+  //   );
+  //   if (message.data) {
+  //     console.log("data:" + JSON.stringify(message.data));
+  //   }
+  //   console.log("with socketId's: " + socket.id + ", " + contact.socket);
+  //   io.to(contact.socket).emit("messageReceived", message);
+  // });
 
   socket.on("robotControl", msg => {
     socket.broadcast.emit("robotControl", msg);
-    console.log(msg);
+    // console.log(msg);
   });
 
   socket.on("disconnect", function() {
